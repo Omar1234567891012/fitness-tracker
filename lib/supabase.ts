b0d14e8b8d9545f8073ea3184c1cd089924724f2
Omar1234567891012 <o.joumaa13@gmail.com>
2026-07-25 13:56:54 +0200

    Debug Cloudflare env

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,3 +6,21 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
+
+import { createBrowserClient } from "@supabase/ssr";
+
+export function createClient() {
+  console.log(
+    "SUPABASE_URL:",
+    process.env.NEXT_PUBLIC_SUPABASE_URL ? "OK" : "MISSING"
+  );
+  console.log(
+    "SUPABASE_KEY:",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? "OK" : "MISSING"
+  );
+
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+}
