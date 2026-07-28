768a9111c552f84b94b6a4211cc545db69c16a1d
Omar1234567891012 <o.joumaa13@gmail.com>
2026-07-28 21:46:43 +0200

    Fix Supabase initialization

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,16 +1,6 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createClient() {
-  console.log(
-    "SUPABASE_URL:",
-    process.env.NEXT_PUBLIC_SUPABASE_URL ? "OK" : "MISSING"
-  );
-
-  console.log(
-    "SUPABASE_KEY:",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? "OK" : "MISSING"
-  );
-
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
